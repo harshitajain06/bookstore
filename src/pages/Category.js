1999d46ae4ae78087661c7ecc017ed3191170c63
Harshita Jain <harshitaforever06@gmail.com>
2023-01-27 17:08:39 +0530

    add  requested changes

--- src/pages/Category.js
+++ src/pages/Category.js
@@ -7,9 +7,11 @@ const Category = () => {
   const status = useSelector((state) => state.category);
 
   return (
-    <div className="flex justify-center align-middle">
-      <button onClick={() => dispatch(checkStatus())} className="bg-blue-500 cursor-pointer mr-20 mt-14 mb-14 text-sm px-2 py-1 text-white font-bold" type="button">CHECK STATUS</button>
-      <h1>{status}</h1>
+    <div>
+      <div className="flex justify-center align-middle">
+        <button onClick={() => dispatch(checkStatus())} className="bg-blue-500 cursor-pointer mr-20 mt-14 mb-14 text-sm px-2 py-1 text-white font-bold" type="button">CHECK STATUS</button>
+      </div>
+      <h1 className=" text-4xl text-center font-bold">{status}</h1>
     </div>
   );
 };
